383f7f75d483ae86f98d5d60a4c613d5362ee24d
Ewout Prangsma <ewout@prangsma.net>
2016-09-02 11:28:37 +0200

    Only fix units that are still invalid after 30sec

--- service/service.go
+++ service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"sync"
+	"time"
 
 	"github.com/ewoutp/go-aggregate-error"
 	"github.com/op/go-logging"
@@ -33,32 +34,82 @@ func NewService(config ServiceConfig, deps ServiceDeps) *Service {
 
 // Run queries all invalid units and tries to get them in the correct state
 func (s *Service) Run() error {
-	invalidUnits := make(chan fleet.FleetUnitState)
+	initialInvalidUnits, err := s.collectInvalidUnits()
+	if err != nil {
+		return maskAny(err)
+	}
+	if len(initialInvalidUnits) == 0 {
+		// No invalud units found
+		return nil
+	}
+
+	s.Logger.Infof("Found %d invalid units, wait a bit for stability", len(initialInvalidUnits))
+	time.Sleep(time.Second * 30)
+
+	// Fetch invalid units again
+	invalidUnitsChan := make(chan fleet.FleetUnitState)
 	var ae errors.AggregateError
 	wg := sync.WaitGroup{}
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		if err := s.Fleet.InvalidUnits(invalidUnits); err != nil {
+		if err := s.Fleet.InvalidUnits(invalidUnitsChan); err != nil {
 			ae.Add(err)
 		}
-		close(invalidUnits)
+		close(invalidUnitsChan)
 	}()
 
-	fix := !s.DryRun
-	for unitState := range invalidUnits {
-		s.Logger.Infof("Found invalid unit: %s", unitState.String())
-		if fix {
-			if err := s.tryFixUnit(unitState); err != nil {
-				s.Logger.Errorf("Failed to fix unit %s", unitState.Name)
+	// Start 5 processors to fix units
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fix := !s.DryRun
+			for unitState := range invalidUnitsChan {
+				_, stable := initialInvalidUnits[unitState.Name]
+				if stable {
+					s.Logger.Infof("Found invalid unit: %s", unitState.String())
+					if fix {
+						if err := s.tryFixUnit(unitState); err != nil {
+							s.Logger.Errorf("Failed to fix unit %s", unitState.Name)
+						}
+					}
+				} else {
+					s.Logger.Infof("Found new invalid unit: %s", unitState.String())
+				}
 			}
-		}
+		}()
 	}
 
 	wg.Wait()
 
 	return maskAny(ae.AsError())
+}
+
+func (s *Service) collectInvalidUnits() (map[string]fleet.FleetUnitState, error) {
+	// Collect invalid units
+	invalidUnitsChan := make(chan fleet.FleetUnitState)
+	invalidUnitsMap := make(map[string]fleet.FleetUnitState)
+	var ae errors.AggregateError
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := s.Fleet.InvalidUnits(invalidUnitsChan); err != nil {
+			ae.Add(err)
+		}
+		close(invalidUnitsChan)
+	}()
+
+	for unitState := range invalidUnitsChan {
+		invalidUnitsMap[unitState.Name] = unitState
+	}
+
+	wg.Wait()
+
+	return invalidUnitsMap, maskAny(ae.AsError())
 }
 
 func (s *Service) tryFixUnit(unit fleet.FleetUnitState) error {
